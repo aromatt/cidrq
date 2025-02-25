f78121fa878e451bf1b9cea1cc6c2fe70adc1b6c
Andrew Matteson <amatteson3@gmail.com>
2025-02-25 16:14:24 -0500

    Add missing pkg/log.go

---
+++ pkg/log.go
@@ -0,0 +1,18 @@
+package pkg
+
+import (
+	"log"
+)
+
+var Logf func(string, ...any) = func(format string, v ...any) {}
+
+func SetVerbose(v bool) {
+	if v {
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+		Logf = func(format string, v ...any) {
+			log.Printf(format, v...)
+		}
+	} else {
+		Logf = func(format string, v ...any) {}
+	}
+}
